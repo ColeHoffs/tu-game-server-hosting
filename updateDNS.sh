b605f8d4c6348f915cd69858fd79be50f3653bc3
cGDeadly <59896970+ColeHoffs@users.noreply.github.com>
2020-09-08 20:50:16 -0500

    Create updateDNS.sh

---
+++ updateDNS.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Shell script to update namecheap.com dynamic dns
+# for a domain to your external IP address
+
+HOSTNAME=YOUR-SUB-DOMAIN (ex: play)
+DOMAIN=YOUR-ROOT-DOMAIN (ex: tu-gaming.com)
+PASSWORD=YOUR_DNS_PASSWORD_FROM_NAMECHEAP
+
+IP=`dig +short your_ngrok_reservered_address_NO_PORT | tail -n1`
+
+curl "https://dynamicdns.park-your-domain.com/update?host=$HOSTNAME&domain=$DOMAIN&password=$PASSWORD&ip=$IP"
+
